ca1cbe6f64a47995a5814e15b7d85d9e7f9a0cb7
dima-leveret <dimazayac@gmail.com>
2022-06-12 18:03:52 +0100

    added folders, can display iamges from folders and assest but not all

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,78 +1,133 @@
 import Headcomponent from "../../components/HeadComponent";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-import { searchAllImages, mapImageResources } from "../../lib/cloudinary";
+import {
+  searchAllImages,
+  mapImageResources,
+  getFolders,
+} from "../../lib/cloudinary";
 
-export default function Gallery({ images: defaultImages, nextCursor: defaultNextCursor }) {
+export default function Gallery({
+  images: defaultImages,
+  nextCursor: defaultNextCursor,
+  folders,
+}) {
   const [images, setImages] = useState(defaultImages);
   const [nextCursor, setNextCursor] = useState(defaultNextCursor);
 
-  console.log('images => ', images);
-  console.log('nextCursor => ', nextCursor);
+  const [activeFolder, setActiveFolder] = useState(defaultNextCursor);
+  console.log(activeFolder);
+
+  // console.log("images => ", images);
+  // console.log("nextCursor => ", nextCursor);
+
+  useEffect(() => {
+    (
+      async function run() {
+        const results = await fetch("/api/searchAllImages", {
+          method: "POST",
+          body: JSON.stringify({
+            nextCursor,
+            max_results: 15,
+            expression: `folder=${activeFolder}`
+          }),
+        }).then((r) => r.json());
+
+        const { resources, next_cursor: updatedNextCursor } = results;
+
+        const images = mapImageResources(resources);
+
+        setImages((prev) => {
+          return [...prev, ...images];
+        });
+
+        setNextCursor(updatedNextCursor);
+      }
+    )()
+  }, [activeFolder])
 
   const handleLoadMoreImages = async (e) => {
-    e.preventDefault()
-    const results = await fetch('/api/searchAllImages', {
+    e.preventDefault();
+    const results = await fetch("/api/searchAllImages", {
       method: "POST",
       body: JSON.stringify({
-        nextCursor
-      })
-    }).then(r => r.json());
+        nextCursor,
+        max_results: 15,
+        expression: `folder=${activeFolder}`,
+      }),
+    }).then((r) => r.json());
 
     const { resources, next_cursor: updatedNextCursor } = results;
 
     const images = mapImageResources(resources);
 
-    setImages(prev => {
-      return [
-        ...prev,
-        ...images
-      ]
+    setImages((prev) => {
+      return [...prev, ...images];
     });
 
     setNextCursor(updatedNextCursor);
-  }
+  };
+
+  const handleOnFolderClick = async (folderPath) => {
+    setActiveFolder(folderPath);
+    setImages([]);
+    setNextCursor(undefined);
+  };
 
   return (
     <div>
       <Headcomponent title="Geleria" />
       <h4>Galeria</h4>
+      <h4>Foldery</h4>
       <div>
-        {
-          images.map(image => {
-            return <img
+        {folders.map((folder) => (
+          <button
+            onClick={() => handleOnFolderClick(folder.path)}
+            key={folder.path}
+          >
+            {" "}
+            {folder.name}{" "}
+          </button>
+        ))}
+      </div>
+      <div>
+        {images.map((image) => {
+          return (
+            <img
               width={300}
               height={300}
               key={image.asset_id}
               src={image.secure_url}
             />
-          })
-        }
+          );
+        })}
       </div>
-      {
-        nextCursor
-        ?
+      {nextCursor ? (
         <button onClick={handleLoadMoreImages}>Pokaż wicej zdjęć</button>
-        :
+      ) : (
         <span> To są wszystkie zdjęcia które obecnie mamy 😃</span>
-      }
+      )}
     </div>
   );
 }
 
-
 export async function getStaticProps() {
-
-  const results = await searchAllImages()
+  const results = await searchAllImages({
+    max_results: 15,
+    expression: 'folder=""'
+  });
 
   const { resources, next_cursor: nextCursor } = results;
 
-  const images = mapImageResources(resources)
+  const images = mapImageResources(resources);
+
+  const { folders } = await getFolders();
 
   return {
     props: {
       images,
-      nextCursor: nextCursor || null
-    }
-  }
+      nextCursor: nextCursor || null,
+      folders,
+    },
+  };
 }
